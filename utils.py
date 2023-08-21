3c82f7adb90ee7df5a12ddeca4e2888dbb841a4d
jung0228 <86190997+jung0228@users.noreply.github.com>
2023-08-21 12:26:24 +0900

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = "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"
+model = "gpt-3.5-turbo"
+
+# 질문 작성하기
+query = "안녕하세요"
+
+def chatgpt(query):
+    
+    # 메시지 설정하기
+    messages = [
+            {"role": "system", "content": "You are a Maths solver."},
+            {"role": "user", "content": query}
+    ]
+
+    # ChatGPT API 호출하기
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages
+    )
+    answer = response['choices'][0]['message']['content']
+    print(answer)
+    return answer 
